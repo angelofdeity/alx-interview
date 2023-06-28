3903dc04f704da6ac3d0e292544b10608e02f228
Nico <lawani.gk@gmail.com>
2023-06-28 17:58:22 +0100

    refactor code to improve efficiency

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,11 @@
 def pascal_triangle(n):
     pascal_t = []
     for i in range(1, n + 1):
-        if i == 1:
-            pascal_t.append([1])
-        elif i == 2:
-            pascal_t.append([1, 1])
-        else:
-            prev = pascal_t[i - 2]
-            new = [1]
-            for j in range(len(prev) - 1):
-                new.append(prev[j] + prev[j + 1])
-            new.append(1)
-            pascal_t.append(new)
+        row = [1] * (i)
+        for j in range(2, i):
+            row[j - 1] = pascal_t[i - 2][j - 2] + pascal_t[i - 2][j - 1]
+        pascal_t.append(row)
     return pascal_t
+
+
+print(pascal_triangle(300))
