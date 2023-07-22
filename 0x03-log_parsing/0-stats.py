53767ca4d197e2fcbd6fd4b29cc8941e69ca2f46
Nico <lawani.gk@gmail.com>
2023-07-22 15:07:55 +0100

    refactor code

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import sys
-import signal
 total_size = count = 0
 # 200, 301, 400, 401, 403, 404, 405 and 500
 codes = {'200': 0, '301': 0, '400': 0, '401': 0,
@@ -9,27 +8,23 @@ codes = {'200': 0, '301': 0, '400': 0, '401': 0,
 
 def print_stats():
     print("File size: {}".format(total_size))
-    for key, value in codes.items():
-        if value:
-            print(key, value)
+    for key, val in codes.items():
+        if val:
+            print("{}: {}".format(key, val))
 
 
 try:
     for line in sys.stdin:
         values = line.split(" ")
-        if len(values) < 4:
-            continue
-        code = values[-2]
-        size = int(values[-1])
-        if code in codes:
-            codes[code] += 1
-        total_size += size
-        count += 1
-        if count % 10 == 0:
-            print_stats()
-except KeyboardInterrupt:
-    print_stats()
-except Exception:
-    pass
+        if len(values) > 4:
+            code = values[-2]
+            size = int(values[-1])
+            if code in codes.keys():
+                codes[code] += 1
+            total_size += size
+            count += 1
+            if count == 10:
+                count = 0
+                print_stats()
 finally:
     print_stats()
